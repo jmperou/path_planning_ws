a2a607a7c475c52b1ec6f40f94389d76c2fbba14
jmperou <74944111+jmperou@users.noreply.github.com>
2022-11-13 12:12:27 -0700

    Add files via upload

---
+++ src/maze_bot/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from glob import glob
+import os
+
+
+package_name = 'maze_bot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jon',
+    maintainer_email='jon@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker_node = maze_bot.talker:main',
+            'subscriber_node = maze_bot.listener:main',
+        ],
+    },
+)
